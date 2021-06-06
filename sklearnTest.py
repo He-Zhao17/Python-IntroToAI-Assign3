de18ef9889f997e425457d8400a1222230092c76
pekokk <zh0208@163.com>
2021-06-06 21:46:11 +0800

    q3 finish

--- sklearnTest.py
+++ sklearnTest.py
@@ -8,7 +8,7 @@ from sklearn import model_selection
 import numpy as np
 from sklearn.model_selection import cross_val_score
 from sklearn.linear_model import LinearRegression
-
+from sklearn.ensemble import RandomForestClassifier
 
 
 cancer = load_breast_cancer()
@@ -21,12 +21,15 @@ print('abrr: ', cancer.feature_names, sep = '\n')
 
 cancerT = tree.DecisionTreeClassifier(criterion = 'gini')
 cancerT = cancerT.fit(cancer.data, cancer.target)
-t1 = LinearRegression()
-scores = cross_val_score(t1, cancer.data, cancer.target, cv=5)
+scores = cross_val_score(cancerT, cancer.data, cancer.target, cv=5)
 print('\nresult with gini:', scores.mean())
 
 cancerT = tree.DecisionTreeClassifier(criterion = 'entropy')
 cancerT = cancerT.fit(cancer.data, cancer.target)
-t1 = LinearRegression()
-scores = cross_val_score(t1, cancer.data, cancer.target, cv=5)
+scores = cross_val_score(cancerT, cancer.data, cancer.target, cv=5)
 print('\nresult with entropy:', scores.mean())
+
+for i in [2, 5, 10]:
+    t1 = RandomForestClassifier(n_estimators = i)
+    t1 = t1.fit(cancer.data, cancer.target)
+
