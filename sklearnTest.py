7718a3829d86c9853318e87f12f23bdd7f95065e
pekokk <zh0208@163.com>
2021-06-06 21:11:32 +0800

    sklearn finish

--- sklearnTest.py
+++ sklearnTest.py
@@ -1,5 +1,32 @@
-import unitest
 import dt
+import sklearn as sk
 
 from sklearn import tree
+from sklearn.datasets import load_breast_cancer
 
+from sklearn import model_selection
+import numpy as np
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LinearRegression
+
+
+
+cancer = load_breast_cancer()
+print("len: ", len(cancer), '\n')
+print('data: ', cancer.data, sep = '\n')
+print('index: ', cancer.target, sep = '\n')
+print('abrr: ', cancer.feature_names, sep = '\n')
+
+
+
+cancerT = tree.DecisionTreeClassifier(criterion = 'gini')
+cancerT = cancerT.fit(cancer.data, cancer.target)
+t1 = LinearRegression()
+scores = cross_val_score(t1, cancer.data, cancer.target, cv=5)
+print('\nresult with gini:', scores.mean())
+
+cancerT = tree.DecisionTreeClassifier(criterion = 'entropy')
+cancerT = cancerT.fit(cancer.data, cancer.target)
+t1 = LinearRegression()
+scores = cross_val_score(t1, cancer.data, cancer.target, cv=5)
+print('\nresult with entropy:', scores.mean())
